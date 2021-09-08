8c485488517797f54e1671bade71918cb60e73b7
Bikach <chakibhoud.io@gmail.com>
2021-09-08 23:56:03 +0200

    first commit

---
+++ src/main/java/com/example/camel_in_action/CamelInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.camel_in_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/camel_in_action/CamelInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camel_in_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
